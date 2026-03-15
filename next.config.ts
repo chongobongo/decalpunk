502e152045866f85b7f16a217a6642f1e04413fa
tyson dolan <tysondolan@hotmail.com>
2026-03-15 11:46:02 -0700

    clerk-3

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    useCache: true,  // enables "use cache" + cacheTag/cacheLife
+  },
 };
 
 export default nextConfig;
