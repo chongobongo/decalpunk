ea9a76ba1914f54c27df94b016a84dee90193b3f
tyson dolan <tysondolan@hotmail.com>
2026-03-20 22:16:58 -0700

    clerk-6

--- app/(public)/stickers/(pages)/custom-stickers/page.tsx
+++ app/(public)/stickers/(pages)/custom-stickers/page.tsx
@@ -1,10 +1,4 @@
-import React from 'react'
-import { CustomStickerForm } from '@/features/custom-stickers/components/CustomStickerForm'
-import { productListData } from '@/features/custom-stickers/data/customStickerData'
-import { customStickerShape } from '@/features/custom-stickers/data/customStickerData'
-import { customStickerMaterialData } from '@/features/custom-stickers/data/customStickerData'
-import { customStickerSizeData } from '@/features/custom-stickers/data/customStickerData'
-import { customStickerQuantityData } from '@/features/custom-stickers/data/customStickerData'
+
 
 const page = () => {
   return (
