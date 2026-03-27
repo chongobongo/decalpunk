a4149de231968e2aac53c398b3d190c4d0ff80b1
tyson dolan <tysondolan@hotmail.com>
2026-03-27 09:34:54 -0700

    errorsubmit

--- features/products/custom-stickers/actions/custom-sticker-actions.ts
+++ features/products/custom-stickers/actions/custom-sticker-actions.ts
@@ -1,8 +1,8 @@
+"use server"
 import z from "zod"
 import { customStickerSchema } from "../schema/customStickerSchema"
 
 export async function createCustomStickerOrder(unsafeData: z.infer<typeof customStickerSchema>) {
-    "use server"
     console.log("server action called")
     return { error: false, message: "test", redirectUrl: "/" }
 }
