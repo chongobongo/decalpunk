f9e2d0d57ff0867ff4df7d18750536f9081be694
tyson dolan <tysondolan@hotmail.com>
2026-03-26 22:32:52 -0700

    table

--- db/tables/stickers-custom-table.ts
+++ db/tables/stickers-custom-table.ts
@@ -1,15 +1,14 @@
 import { pgTable, text, uuid } from "drizzle-orm/pg-core";
 import { id } from "../schemaHelpers";
 
-
 export const StickersCustomTable = pgTable("stickers_custom", {
     id,
-    job_order_id: uuid(),
-    product: text(),
-    shape: text(),
-    material: text(),
-    finish: text(),
-    size: text(),
-    quantity: text(),
-    img: text()
+    job_order_id: uuid("job_order_id"),
+    product: text("product"),
+    shape: text("shape"),
+    material: text("material"),
+    finish: text("finish"),
+    size: text("size"),
+    quantity: text("quantity"),
+    img: text("img")
 })
