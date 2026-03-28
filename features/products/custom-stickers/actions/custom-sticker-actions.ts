40392ad9d32c763d4aee0b559268cc6766bf7e7d
tyson dolan <tysondolan@hotmail.com>
2026-03-27 20:11:07 -0700

    supa2

--- features/products/custom-stickers/actions/custom-sticker-actions.ts
+++ features/products/custom-stickers/actions/custom-sticker-actions.ts
@@ -7,6 +7,7 @@ import { canCreateOrder } from "@/app/permissions/general"
 
 export async function createCustomStickerOrder(unsafeData: z.infer<typeof customStickerSchema>) {
     const user = await getCurrentUser()
+        console.log("user:", JSON.stringify(user))
     if (user.userId == null || !canCreateOrder(user)) {
         return { error: true, message: "You do not have permission to create an order." }
     }
