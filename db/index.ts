2911f22c9921583e78a0d4a9107af07dc90aa10e
tyson dolan <tysondolan@hotmail.com>
2026-03-27 20:51:30 -0700

    supa5

--- db/index.ts
+++ db/index.ts
@@ -1,15 +1,12 @@
-// DB Connectiion
-// C:\Users\tyson\OneDrive\Desktop\Cinegraph\cinegraph\src\db\index.ts
-
 import { config } from 'dotenv';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from "./schemaDB"
 
-config({ path: '.env' }); // or .env.local
+config({ path: '.env' });
 
 const connectionString = process.env.DATABASE_URL ||
-"postgress://localhost:5432/drizzle";
+"postgres://localhost:5432/drizzle";
 
-const client = postgres(connectionString);
+const client = postgres(connectionString, { prepare: false });
 export const db = drizzle(client, { schema });
